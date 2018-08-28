abcabcf610a5a5357ac6b58097f1da0dea2138bf
Chester Black <chesterblack@Chesters-MacBook-Pro.local>
2018-08-28 18:47:27 +0100

    created database connect function

--- redirector.php
+++ redirector.php
@@ -5,12 +5,35 @@
 
     class redirector{
 
- 
-        function connect(){
+        public $pdo;
+        public $status;
 
+        //Connect to the database
+        function connect(){
+            try {
+                $host = $details->$host;
+                $name = $details->$name;
+                $user = $details->$user;
+                $pass = $details->$pass;
+
+                //Create a new PDO to connect to database
+                $this->pdo = new PDO("mysql:host=$host;dbname=$name", $user, $pass);
+
+                //Throw an exception if error occurs so that the catch works if something goes wrong
+                $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                $this->status = "PDO created";
+
+            } catch (PDOException $e) {
+                $this->status = "PDO failed";
+
+                //Echoes error message
+                echo $this->status . $e->getMessage();
+            }
+            return $this->pdo;
         }
         
-        function doesStringExist(){
+        function doesStringExist($string){
             
         }
 
