a819feaaa0c0de6656ec10b1b6b6e2b001798c29
Chester Black <chesterblack@Chesters-MacBook-Pro.local>
2018-08-28 18:47:02 +0100

    set up files and laid out functions that will be needed

---
+++ redirector.php
@@ -0,0 +1,26 @@
+<?php
+
+    //Get database details that haven't been committed to git
+    require_once "details.php";
+
+    class redirector{
+
+ 
+        function connect(){
+
+        }
+        
+        function doesStringExist(){
+            
+        }
+
+        function enterData(){
+        }
+        
+        function generateString(){
+        }
+
+    }
+
+    $redirector = new redirector();
+?>
