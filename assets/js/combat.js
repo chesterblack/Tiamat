df1884393295a917792167f76985bcd631b48cc3
Chester Black <chesterblack@Chesters-MacBook-Pro.local>
2018-10-01 00:53:59 +0100

    created damage function

--- assets/js/combat.js
+++ assets/js/combat.js
@@ -1,22 +1,29 @@
 console.log("hello world");
 
 var characters = [];
+var characterName = "";
 
-function formChanged(){
+function newCharacter(){
     characters[characters.length] = {
-        name:document.getElementsByName("nameInput")[0].value,init:document.getElementsByName("initInput")[0].value
+        name:document.getElementsByName("nameInput")[0].value,init:document.getElementsByName("initInput")[0].value,
+        health:document.getElementsByName("healthInput")[0].value
     };
-
-    console.log(characters);
 
     var charactersLength = characters.length;
 
     var display = "<ul>";
-
     for (i = 0; i < charactersLength; i++){
-        display += "<li>NAME: " + characters[i].name + "<br /> INITIATIVE: " + characters[i].init + "</li><hr>";
+        display += "<li>NAME: " + characters[i].name + "<br /> INITIATIVE: " + characters[i].init + "<br /> HEALTH:" + characters[i].health + "<input type='text' name='damage'><button onclick='healthEdit(i)'>DAMAGE</button></li><hr>";
     }
     display += "</ul>";
 
     document.getElementById("order").innerHTML = display;
-};
+}
+
+function healthEdit(i){
+    var damageAmount = document.getElementsByName("damage")[0].value;
+
+    var newHealth = characters[i].health - damageAmount;
+
+    characters[i].health = newHealth;
+}
