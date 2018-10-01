ddedefd5e9bfdd68b0d0c0697fcffe42d0041b90
Chester Black <chesterblack@Chesters-MacBook-Pro.local>
2018-10-01 01:00:44 +0100

    seperated displayCharacters into it's own function

--- assets/js/combat.js
+++ assets/js/combat.js
@@ -9,21 +9,29 @@ function newCharacter(){
         health:document.getElementsByName("healthInput")[0].value
     };
 
-    var charactersLength = characters.length;
-
-    var display = "<ul>";
-    for (i = 0; i < charactersLength; i++){
-        display += "<li>NAME: " + characters[i].name + "<br /> INITIATIVE: " + characters[i].init + "<br /> HEALTH:" + characters[i].health + "<input type='text' name='damage'><button onclick='healthEdit(i)'>DAMAGE</button></li><hr>";
-    }
-    display += "</ul>";
-
-    document.getElementById("order").innerHTML = display;
+    displayCharacters();
 }
 
 function healthEdit(i){
+    console.log("DAMANGED");
+
     var damageAmount = document.getElementsByName("damage")[0].value;
 
     var newHealth = characters[i].health - damageAmount;
 
     characters[i].health = newHealth;
+
+    displayCharacters();
+}
+
+function displayCharacters(){
+    var charactersLength = characters.length;
+
+    var display = "<ul>";
+    for (i = 0; i < charactersLength; i++){
+        display += "<li>NAME: " + characters[i].name + "<br /> INITIATIVE: " + characters[i].init + "<br /> HEALTH:" + characters[i].health + "<input type='text' name='damage'><button onclick='healthEdit("+i+")'>DAMAGE</button></li><hr>";
+    }
+    display += "</ul>";
+
+    document.getElementById("order").innerHTML = display;
 }
