2156e83cd88669b6bc8344b353e9bf9e7f769cc1
Chester Black <chesterblack@Chesters-MacBook-Pro.local>
2018-10-02 18:53:48 +0100

    set up ordering of characters

--- assets/js/combat.js
+++ assets/js/combat.js
@@ -41,53 +41,25 @@ function displayCharacters(){
     document.getElementById("order").innerHTML = display;
 }
 
-function test(){
-    var testArray = [];
-    characters = testArray;
-    console.log(testArray);
-}
-
 function orderCharacters(){
-    console.log(characters);
+var orderedCharacters = [];
 
-    var charactersLength = characters.length;
-    var orderedCharacters = [];
-
-    for (i = 0; i < charactersLength; i++){
-        var originTest = characters[i].init;
-
-        try{
-            var newTest = characters[i+1].init;
-        } catch {
-            console.log("break!");
-            break;
-        }
-        
-        if(originTest > newTest){
-            orderedCharacters.push(characters[i]);
-            console.log(originTest + " larger than " + newTest);
-            console.log(orderedCharacters);
-            console.log("---");
-
-        } else {
-            orderedCharacters.unshift(characters[i+1]);
-            console.log(originTest + " smaller than " + newTest);
-            console.log(orderedCharacters);
-            console.log("---");
-
-        }
-        
+    function compare(a,b) {
+        if (a.init < b.init)
+            return -1;
+        if (a.init > b.init)
+            return 1;
+        return 0;
     }
 
-    console.log(orderedCharacters);
+    orderedCharacters = characters.sort(compare);
+    var orderedCharactersLength = orderedCharacters.length;
 
-    // var orderedCharactersLength = orderedCharacters.length;
+    var display = "<ul>";
+    for (i = 0; i < orderedCharactersLength; i++){
+        display += "<li>NAME: " + orderedCharacters[i].name + "<br /> INITIATIVE: " + orderedCharacters[i].init + "<br /> HEALTH: " + orderedCharacters[i].health + "<input type='text' name='damage'><button onclick='healthEdit("+i+")'>DAMAGE</button></li><hr>";
+    }
+    display += "</ul>";
 
-    // var display = "<ul>";
-    // for (i = 0; i < orderedCharactersLength; i++){
-    //     display += "<li>NAME: " + orderedCharacters[i].name + "<br /> INITIATIVE: " + orderedCharacters[i].init + "<br /> HEALTH: " + orderedCharacters[i].health + "<input type='text' name='damage'><button onclick='healthEdit("+i+")'>DAMAGE</button></li><hr>";
-    // }
-    // display += "</ul>";
-
-    // document.getElementById("order").innerHTML = display;
+    document.getElementById("order").innerHTML = display;  
 }
