8c398f3425cfb87bbb88cb9e7a612817af59fdfe
Chester Black <chesterblack@Chesters-MacBook-Pro.local>
2018-12-14 22:03:08 +0000

    added subraces to builder form through ajax

---
+++ assets/php/ajax-character-builder.php
@@ -0,0 +1,18 @@
+<?php
+    require_once $_SERVER['DOCUMENT_ROOT']."/assets/php/character-generator.php";
+
+    error_reporting(E_ALL);
+
+    $mainrace = $_REQUEST['q'];
+
+    $races = $process->getRaces("subrace_name",$mainrace);
+
+    print_r($races);
+
+    if($races['subrace_name'] !== "nope"){
+        foreach($races as $race){
+            $subrace_name = $race['subrace_name'];
+            echo "<option value='$subrace_name'>$subrace_name</option>\n";
+        }
+    }
+?>
