9b94af8eff1cbb582e31d6feed913d2737d54489
Chester Black <chesterblack@Chesters-MacBook-Pro.local>
2019-02-10 14:26:29 +0000

    added functionality for choosing a weapon in the builder form

---
+++ assets/php/ajax-weapons.php
@@ -0,0 +1,23 @@
+<?php
+    require_once $_SERVER['DOCUMENT_ROOT']."/assets/php/character-generator.php";
+
+    error_reporting(E_ALL);
+
+    $weaponName = $_REQUEST['q'];
+
+    $weapons = $process->getEquipment("weapons","*",$weaponName);
+
+    foreach($weapons as $weapon){
+        $type = $weapon['proficiency']." ".$weapon['type'];
+        $damage = $weapon['damage']." ".$weapon['damage_type'];
+        $weight = $weapon['weight'];
+        $price = $weapon['price'];
+        $properties = $weapon['properties'];
+        // if($weapon['ranges'] != '0'){
+            $range = $weapon['ranges'];
+        // }
+
+        echo "<td>$type</td><td>$damage</td><td>$weight</td><td>$price</td><td>$properties</td><td>$range</td>";
+
+    }
+?>
