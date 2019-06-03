b3ee1b16e8164dcd6fa09e23959f27970edd371e
Chester Black <chesterblack@chesters-mbp.home>
2019-06-03 23:56:24 +0100

    added armour to character builder

--- assets/php/ajax-weapons.php
+++ assets/php/ajax-weapons.php
@@ -3,23 +3,59 @@
 
     error_reporting(E_ALL);
 
-    $weaponName = $_REQUEST['q'];
+    $itemName = $_REQUEST['q'];
+    $table = $_REQUEST['table'];
 
-    $weapons = $process->getEquipment("weapons","*",$weaponName);
+    $items = $process->getEquipment($table,"*",$itemName);
     
-    foreach($weapons as $weapon){
-        $name = $weapon['name'];
-        $type = $weapon['proficiency']." ".$weapon['type'];
-        $damage = $weapon['damage']." ".$weapon['damage_type'];
-        $weight = $weapon['weight'];
-        $price = $weapon['price'];
-        $properties = $weapon['properties'];
-        // if($weapon['ranges'] != '0'){
-            $range = $weapon['ranges'];
-        // }
+    switch($table){
+        case "weapons":
+            foreach($items as $item){
+                $name = $item['name'];
+                $type = $item['proficiency']." ".$item['type'];
+                $damage = $item['damage']." ".$item['damage_type'];
+                $weight = $item['weight'];
+                $price = $item['price'];
+                $properties = $item['properties'];
+                $range = $item['ranges'];
 
+                echo "<td name='weapon' value='$name'>$name</td><td>$type</td><td>$damage</td><td>$weight lbs</td><td>$price</td><td>$properties</td><td>$range</td>";
+            };
+            break;
+        case "armour":
+            foreach($items as $item){
+                $name = $item['name'];
+                $type = $item['type'];
+                switch($type){
+                    case "Light":
+                        $dexBonus = '∞';
+                        break;
+                    case "Medium":
+                        $dexBonus = 2;
+                        break;
+                    case "Heavy":
+                        $dexBonus = 0;
+                        break;
+                    case "Shield":
+                        $dexBonus = 0;
+                        break;
+                    default:
+                        $dexBonus = 0;
+                }
+                if($dexBonus){
+                    $ac = $item['base_ac']."+DEX(".$dexBonus.")";
+                }else{
+                    $ac = $item['base_ac'];
+                }
+                $weight = $item['weight'];
+                $price = $item['price'];
+                $minStrength = $item['min_strength'];
+                $stealth = $item['stealth'];
 
-
-        echo "<td name='weapon' value='$name'>$name</td><td>$type</td><td>$damage</td><td>$weight</td><td>$price</td><td>$properties</td><td>$range</td>";
+                echo "<td name='armour' value='$name'>$name</td><td>$type</td><td>$ac</td><td>$weight lbs</td><td>$price</td><td id='minStr'>$minStrength</td><td>$stealth</td>";
+            };
+            break;
+        default:
+            echo "ERROR with switch statement in AJAX call - invalid table name provided.";
     }
 ?>
