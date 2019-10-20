29d2147f60855349c612deef07820d5a4e464ec4
Chester Black <chesterblack@chesters-mbp.home>
2019-10-20 16:23:13 +0100

    can edit character using query string

--- assets/php/ajax-classes.php
+++ assets/php/ajax-classes.php
@@ -3,14 +3,21 @@
 
     error_reporting(E_ALL);
 
-    $mainclass = $_REQUEST['q'];
+    $mainclass = $_GET['q'];
 
     $classes = $process->getClasses("subclass_name",$mainclass);
 
-    // print_r($classes);
-
     foreach($classes as $class){
         $subclass_name = $class['subclass_name'];
-        echo "<option value='$subclass_name'>".str_replace('_', ' ', $subclass_name)."</option>\n";
+
+        if (isset($_GET['editing'])) {
+            if ($_GET['editing'] == $subclass_name) {
+                echo "<option value='$subclass_name' selected>".str_replace('_', ' ', $subclass_name)."</option>\n";
+            } else {
+                echo "<option value='$subclass_name'>".str_replace('_', ' ', $subclass_name)."</option>\n";
+            }
+        } else {
+            echo "<option value='$subclass_name'>".str_replace('_', ' ', $subclass_name)."</option>\n";
+        }
     }
 ?>
--- assets/php/ajax-races.php
+++ assets/php/ajax-races.php
@@ -3,14 +3,23 @@
 
     error_reporting(E_ALL);
 
-    $mainrace = $_REQUEST['q'];
+    $mainrace = $_GET['q'];
 
     $arrays = $process->getRaces("subrace_name",$mainrace);
 
     foreach ($arrays as $array){
         if(is_array($array)){
             $subrace_name = $array['subrace_name'];
-            echo "<option value='$subrace_name'>$subrace_name</option>\n";
+
+            if (isset($_GET['editing'])) {
+                if ($_GET['editing'] == $subrace_name) {
+                    echo "<option value='$subrace_name' selected>$subrace_name</option>\n";
+                } else {
+                    echo "<option value='$subrace_name'>$subrace_name</option>\n";
+                }
+            } else {
+                echo "<option value='$subrace_name'>$subrace_name</option>\n";
+            }
         }
     }
-    ?>
+?>
